507e195d1cf9c37a9e7d0dbcbfe3d0e4cef9fa39
UPENDRA SINGH <102982552+upendra-eth@users.noreply.github.com>
2023-11-24 17:55:33 +0530

    Update index.js
    updated console massages

--- index.js
+++ index.js
@@ -1,66 +1,63 @@
 const TelegramBot = require("node-telegram-bot-api");
-const Configuration = require("openai");
 const OpenAIApi = require("openai");
-
 const { config } = require("dotenv");
+
 config();
 
 const TOKEN = process.env.TELEGRAM_TOKEN;
+const bot = new TelegramBot(TOKEN, { polling: true });
+let openai;
+
+// Initialize OpenAI API
+const initOpenAI = () => {
+  try {
+    openai = new OpenAIApi({
+      apiKey: process.env.OPENAI_API_KEY,
+    });
+  } catch (e) {
+    console.log("Error initializing OpenAI:", e);
+  }
+};
+initOpenAI();
+
 console.log(
-  TelegramBot,
   "------------------------- running open ai chat bot service ------------------------------"
 );
-const bot = new TelegramBot(TOKEN, { polling: true });
-let firstMsg = true;
-var openai;
+
 bot.on("message", (message) => {
-  if (firstMsg) {
+  if (message.text.startsWith("/prompt")) {
+    const chatId = message.chat.id;
+    const messageText = message.text.replace("/prompt", "").trim();
+
+    console.log("Asked:", messageText);
+
+
+    if (!openai) {
+      bot.sendMessage(
+        chatId,
+        "OpenAI not initialized yet. Please try again later."
+      );
+      return;
+    }
+
+    openai.chat.completions
+      .create({
+        model: "gpt-3.5-turbo",
+        messages: [{ role: "user", content: messageText }],
+      })
+      .then((res) => {
+        const result = res.choices[0].message.content;
+        console.log("Response:", result);
+        bot.sendMessage(chatId, result);
+      })
+      .catch((err) => {
+        console.log("Error generating response:", err);
+        bot.sendMessage(chatId, "An error occurred. Please try again later.");
+      });
+  } else {
     bot.sendMessage(
       message.chat.id,
-      `Hello ${message.chat.first_name}, use "/prompt" followed by your query`
+      `Hello ${message.from.first_name}, use "/prompt" followed by your query`
     );
-    firstMsg = false;
   }
-  console.log("question asked  ---------------------- ", message.text);
-  sendMessage();
 });
-
-const sendMessage = () => {
-  bot.onText(/\/prompt (.+)/, (msg, match) => {
-    const chatId = msg.chat.id;
-    const messageText = match[1];
-    try {
-      openai.chat.completions
-        .create({
-          model: "gpt-3.5-turbo",
-          messages: [{ role: "user", content: messageText }],
-        })
-        .then((res) => {
-          const result = res.choices[0].message.content;
-          console.log("response ---------------------- ", result);
-          bot.sendMessage(chatId, result);
-        });
-    } catch (e) {
-      console.log(
-        "----------------------------------- error ----------------------------------",
-        e
-      );
-    }
-  });
-};
-
-const hello = () => {
-  try {
-    openai = new OpenAIApi(
-      new Configuration({
-        apiKey: process.env.OPENAI_API_KEY,
-      })
-    );
-  } catch (e) {
-    console.log(
-      "--------------------------------- error -------------------------------------",
-      e
-    );
-  }
-};
-hello();
